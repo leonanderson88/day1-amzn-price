ab021f2e203c78704a17798a6e57131b3fe9bf0e
Leon Anderson <lleoand@amazon.com>
2025-04-23 12:18:37 +1000

    Update incorrect updateURL and downloadURL

--- day1-amzn-price.user.js
+++ day1-amzn-price.user.js
@@ -4,8 +4,8 @@
 // @version       2025-04-23
 // @description   Script to display day 1 stock price for an Amazoian on their PhoneTool page. (Uses Yahoo! Finance's download endpoint that provides a JSON of the stock quote.). Based on the original AMZN on PT plugin developed by Sumeet Mulani <mulanis@amazon.com>
 // @author        Leon Anderson <lleoand@amazon.com>
-// @updateURL     https://github.com/leonanderson88/grimsby-csv-exporter/raw/refs/heads/main/grimsby-csv-exporter.user.js
-// @downloadURL   https://github.com/leonanderson88/grimsby-csv-exporter/raw/refs/heads/main/grimsby-csv-exporter.user.js
+// @updateURL     https://github.com/leonanderson88/day1-amzn-price/raw/refs/heads/main/day1-amzn-price.user.js
+// @downloadURL   https://github.com/leonanderson88/day1-amzn-price/raw/refs/heads/main/day1-amzn-price.user.js
 // @match         https://phonetool.amazon.com/users/*
 // @exclude       https://phonetool.amazon.com/users/*/org
 // @icon          data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABVUlEQVRYhe2XMS9DURTHf1ckLxJNNWGqNC9CxIKBuSXsnRjs1aFDv4DJF6gNg1gMTD6Aod0NZWnE0oGlDCXE0t5jaDQE950mmg7uSe7wkt8753f/Z3nPoKl9WQCKQIgh42SFMoYmhhI5U4lqbSKHH0gcoYohVMl+lVkkb65cyLCizTGGcPT+konrE+L1shN+CjM8zG/xklwCKAErLl6TgADMnG4w8nij8IW38VluN886D9vGOUOTAABBo4YF7tZ2ac5lf2TGaudMXuwQNGratgxpQRFBRHhNLv/KPE+tdjltqROw1gIwfbTu5tSjexTo5VZ9EfhI4P8K+BX4BAYu4FcwcAG/Ap/AwAX8CnwC/RJQf5bbRKr7yR11bCL1pwJVgFa6QDuIYa11nnYQo5UudN4U6lHNo3/NDiWNUFaIfi+hSN7suZDoBHKmgpDV3OZTNTXDAd4Bh5nLjwk9nN8AAAAASUVORK5CYII=
